14c00332bc09b86ec6d738e369caa89a3a8a3ed1
mistersecret312 <sergiigamer7@gmail.com>
2023-12-28 17:18:13 +0200

    4 New Upgrades

--- fabric/src/main/java/net/tapparitions/fabric/ModEvents.java
+++ fabric/src/main/java/net/tapparitions/fabric/ModEvents.java
@@ -1,5 +1,12 @@
 package net.tapparitions.fabric;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityEvent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.tapparitions.upgrades.TAUpgrades;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
 import whocraft.tardis_refined.registry.DimensionTypes;
@@ -15,10 +22,64 @@ public class ModEvents {
                 TardisLevelOperator operator = TardisLevelOperator.get(world).get();
                 if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.HEALING_I.get())){
                     operator.getLevel().getServer().getLevel(operator.getLevel().dimension()).players().forEach(player -> {
-                        player.heal(1f);
+                        if(operator.getLevel().getGameTime() % 400 == 0) {
+                            player.heal(2f);
+                        }
                     });
-                };
+                }
+                if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.HEALING_II.get())){
+                    operator.getLevel().getServer().getLevel(operator.getLevel().dimension()).players().forEach(player -> {
+                        if(operator.getLevel().getGameTime() % 20 == 0) {
+                            if(player.getActiveEffectsMap().containsKey(MobEffects.DIG_SLOWDOWN)) {
+                                player.removeEffect(MobEffects.DIG_SLOWDOWN);
+                            }
+                            if(player.getActiveEffectsMap().containsKey(MobEffects.MOVEMENT_SLOWDOWN)) {
+                                player.removeEffect(MobEffects.MOVEMENT_SLOWDOWN);
+                            }
+                            if(player.getActiveEffectsMap().containsKey(MobEffects.WITHER)) {
+                                player.removeEffect(MobEffects.WITHER);
+                            }
+                            if(player.getActiveEffectsMap().containsKey(MobEffects.WEAKNESS)) {
+                                player.removeEffect(MobEffects.WEAKNESS);
+                            }
+                            if(player.getActiveEffectsMap().containsKey(MobEffects.POISON)) {
+                                player.removeEffect(MobEffects.POISON);
+                            }
+                        }
+                    });
+                }
             }
+        });
+        ServerLivingEntityEvents.ALLOW_DEATH.register((entity, source, amount) -> {
+            Level world = entity.level();
+            if (world instanceof ServerLevel serverLevel) {
+                if (world.dimensionTypeId().location() == DimensionTypes.TARDIS.location()) {
+                    TardisLevelOperator operator = TardisLevelOperator.get(serverLevel).get();
+                    if (operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.PROTECTION_I.get()) && entity instanceof Player) {
+                        return entity.getHealth()<amount;
+                    }
+                }
+            }
+            return true;
+        });
+
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register((entity, source, amount) -> {
+            Level world = entity.level();
+            if (world instanceof ServerLevel serverLevel) {
+                if (world.dimensionTypeId().location() == DimensionTypes.TARDIS.location()) {
+                    TardisLevelOperator operator = TardisLevelOperator.get(serverLevel).get();
+                    if (operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.ER_I.get())) {
+                        if ((entity.getHealth()-amount) / entity.getMaxHealth() < 0.3 && entity instanceof Player) {
+                            if((entity.getHealth()-amount)/entity.getMaxHealth() < 0 && !operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.PROTECTION_I.get())){
+                                amount /= 2;
+                            }
+                            entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 80, 5, false, false, false));
+                        }
+                    }
+                }
+            }
+
+            return true;
         });
     }
 
--- forge/src/main/java/net/tapparitions/neoforge/events/CommonBus.java
+++ forge/src/main/java/net/tapparitions/neoforge/events/CommonBus.java
@@ -1,10 +1,17 @@
 package net.tapparitions.neoforge.events;
 
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.tapparitions.TApparitionsMod;
 import net.tapparitions.upgrades.TAUpgrades;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
@@ -22,8 +29,57 @@ public class CommonBus {
                     TardisLevelOperator operator = TardisLevelOperator.get(serverLevel).get();
                     if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.HEALING_I.get())){
                         operator.getLevel().getServer().getLevel(operator.getLevel().dimension()).players().forEach(player -> {
-                            player.heal(1f);
+                            if(operator.getLevel().getGameTime() % 400 == 0) {
+                                player.heal(2f);
+                            }
                         });
+                    }
+                    if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.HEALING_II.get())){
+                        operator.getLevel().getServer().getLevel(operator.getLevel().dimension()).players().forEach(player -> {
+                            if(operator.getLevel().getGameTime() % 20 == 0) {
+                                if(player.getActiveEffectsMap().containsKey(MobEffects.DIG_SLOWDOWN)) {
+                                    player.removeEffect(MobEffects.DIG_SLOWDOWN);
+                                }
+                                if(player.getActiveEffectsMap().containsKey(MobEffects.MOVEMENT_SLOWDOWN)) {
+                                    player.removeEffect(MobEffects.MOVEMENT_SLOWDOWN);
+                                }
+                                if(player.getActiveEffectsMap().containsKey(MobEffects.WITHER)) {
+                                    player.removeEffect(MobEffects.WITHER);
+                                }
+                                if(player.getActiveEffectsMap().containsKey(MobEffects.WEAKNESS)) {
+                                    player.removeEffect(MobEffects.WEAKNESS);
+                                }
+                                if(player.getActiveEffectsMap().containsKey(MobEffects.POISON)) {
+                                    player.removeEffect(MobEffects.POISON);
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onDamagePlayer(LivingDamageEvent event){
+        Level level = event.getEntity().level();
+        LivingEntity entity = event.getEntity();
+        if (level instanceof ServerLevel serverLevel) {
+            if (level.dimensionTypeId().location() == DimensionTypes.TARDIS.location()) {
+                TardisLevelOperator operator = TardisLevelOperator.get(serverLevel).get();
+                if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.PROTECTION_I.get())) {
+                    if (event.getEntity() instanceof Player) {
+                        if(event.getAmount()>entity.getHealth()){
+                            event.setCanceled(true);
+                        }
+                    }
+                }
+                if(operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.ER_I.get())){
+                    if ((entity.getHealth()-event.getAmount()) / entity.getMaxHealth() < 0.1 && entity instanceof Player) {
+                        if((entity.getHealth()-event.getAmount())/entity.getMaxHealth() < 0 && !operator.getUpgradeHandler().isUpgradeUnlocked(TAUpgrades.PROTECTION_I.get())){
+                            event.setAmount(event.getAmount()/2);
+                        }
+                        entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 80, 5, false, false, false));
                     }
                 }
             }
